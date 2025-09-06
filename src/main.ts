2569df297f12f3a4f9622d4782ae4fe1de009f26
Josue Perez Valenzuela <josueperezv2004@gmail.com>
2025-09-06 01:18:17 -0400

    Inicio de auth

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,39 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule, { bufferLogs: true });
 
-    const config = app.get(ConfigService);
-    await app.listen(config.get<number>('APP_PORT') || 3000);
+    app.useGlobalPipes(
+        new ValidationPipe({
+            whitelist: true, // quita propiedades no declaradas en DTOs
+            forbidNonWhitelisted: true, // si viene “basura”, lanza 400
+            transform: true, // castea (string→number, etc.)
+            transformOptions: { enableImplicitConversion: true },
+        })
+    );
+
+    app.enableCors({ origin: true, credentials: true });
+
+    const config = new DocumentBuilder()
+        .setTitle('UMSS Reservas API')
+        .setDescription('API de reservas de ambientes')
+        .setVersion('0.1.0')
+        .addBearerAuth() // uso JWT
+        .build();
+
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('docs', app, document);
+    // http://localhost:3000/docs
+
+    const cfg = app.get(ConfigService);
+    const port = cfg.get<number>('APP_PORT') ?? 3000;
+    await app.listen(port);
+
+    console.log(`API ready on http://localhost:${port}`);
+    console.log(`Swagger on http://localhost:${port}/docs`);
 }
 bootstrap();
