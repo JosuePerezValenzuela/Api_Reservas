571fe09e47d85416fd66454962e6daabed98876d
Josue Perez Valenzuela <josueperezv2004@gmail.com>
2025-09-08 10:28:31 -0400

    Mejora del seeder person_roles

--- src/seeds/10_people/103_person_roles_seed.ts
+++ src/seeds/10_people/103_person_roles_seed.ts
@@ -1,20 +1,44 @@
 import { Seeder } from "../seed-runner";
 
+interface RoleRow {
+    id_role: number;
+}
+
+interface FacultyRow {
+    id_faculty: number;
+}
+
 export const seedPersonRoles: Seeder = {
     name: '10_people:person_roles',
     run: async (ds) => {
-        await ds
-            .createQueryBuilder()
-            .insert()
-            .into('person_roles')
-            .values([
-                {
-                    ci_person: '9315951',
-                    id_role: 1,
-                    id_faculty: null, // Es el admin general
-                }
-            ])
-            .orIgnore()
-            .execute();
-    }
+        // Buscamos el id_rol que queremos asignar
+        const roleDoc = await ds.query<RoleRow[]>(
+            `SELECT id_role
+            FROM roles
+            WHERE role_name = 'DOCENTE'
+            LIMIT 1`,
+        );
+
+        if(!roleDoc.length) throw new Error('No se encontro el rol DOCENTE');
+
+        const fcyt = await ds.query<FacultyRow[]>(
+            `SELECT id_faculty
+            FROM faculties
+            WHERE lower(btrim(faculty_name)) = lower(btrim($1))
+            LIMIT 1`,
+            ['FCyT'],
+        );
+
+        if(!fcyt.length) throw new Error('No se encontro la facultad FCyT');
+
+        const idRole = roleDoc[0].id_role;
+        const idFaculty = fcyt[0].id_faculty;
+
+        await ds.query(
+            `INSERT INTO person_roles (ci_person, id_role, id_faculty)
+            VALUES ($1, $2, $3)
+            ON CONFLICT (ci_person, id_role, id_faculty) DO NOTHING`,
+            ['9315951', idRole, idFaculty],
+        );
+    },
 };
